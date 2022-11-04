d355d52424312d16353903c07cd4c19a861a29ac
Rahul-Jayakumar <77782918+Rahul-Jayakumar@users.noreply.github.com>
2022-11-04 07:12:17 +0200

    Started Fundamentals Pt.1 Assignments-until Data Types

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,14 @@
+//LECTURE: VALUES AND VARIABLES
+let country = "South Africa"
+let continent = "Africa"
+let population = "55 000 000"
+
+console.log("My country is " + country + ", my continent is " + continent + " and it's population is " + population)
+
+//LECTURE: DATA TYPES
+
+let isIsland = false
+let language = ""
+console.log({ isIsland, population, country, language })
+
+//
