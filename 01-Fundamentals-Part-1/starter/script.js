43645ce904a27b50b473191ff33bc5c11fb4ef34
root <jayakumarrahul8@gmail.com>
2022-11-06 16:52:06 +0000

    Variables assignment added

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,14 +1,22 @@
 //LECTURE: VALUES AND VARIABLES
-let country = "South Africa"
-let continent = "Africa"
-let population = "55 000 000"
+const country = "South Africa";
+const continent = "Africa";
+let population = 55000000;
 
 console.log("My country is " + country + ", my continent is " + continent + " and it's population is " + population)
 
 //LECTURE: DATA TYPES
 
-let isIsland = false
-let language = ""
-console.log({ isIsland, population, country, language })
+const isIsland = false;
+const language = "";
+console.log({ isIsland, population, country, language });
 
-//
+//LECTURE: Basic Operators
+halfpopulation = population/2;
+console.log(halfpopulation);
+console.log(population + 1);
+console.log(country + ' has more people than Finland.')
+console.log(country + "has more than 33000000 people.")
+
+
+
